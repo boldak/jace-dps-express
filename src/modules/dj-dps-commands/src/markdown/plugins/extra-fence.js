44f4b2d1f9a906e06bce66fc26e592c323332c64
boldak <boldak.andrey@gmail.com>
2021-02-27 00:04:12 +0200

    change md

--- src/modules/dj-dps-commands/src/markdown/plugins/extra-fence.js
+++
@@ -1,8 +0,0 @@
-const markdownitfence = require('markdown-it-fence')
-
-module.exports = (md, opts) => markdownitfence(
-	md, 
-	'extra-fence', 
-	{
-    	render: (tokens, idx) => tokens[idx].content.trim()
-  	})
